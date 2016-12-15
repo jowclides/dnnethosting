1f7a527a66e45dd196b72d0c8e410f99996d0f79
Emerson Pereira da Silva <emerson.pereira@gmail.com>
2016-12-15 13:57:47 +0000

    Changes made by Emerson Pereira da Silva (emerson.pereira@gmail.com). Mudança de nome de App...

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,7 @@
     document.addEventListener("deviceready", function () {
         var app = new kendo.mobile.Application(document.body, { skin: "nova" });
         $("#images").kendoMobileListView({
-            dataSource: ["images/01.jpg", "images/02.jpg", "images/03.jpg", "images/04.jpg", "images/05.jpg", "images/06.jpg", "images/07.jpg"],
+            dataSource: ["images/Ryan-001.jpeg", "images/Ryan-01.jpg", "images/Ryan-02.jpeg", "images/Ryan-03.jpeg", "images/Ryan-04.jpeg", "images/Ryan-05.jpeg", "images/Ryan-06.jpeg", "images/Ryan-07.jpeg", "images/Ryan-08.jpeg", "images/Ryan-09.jpeg", "images/Ryan-10.jpeg", "images/Ryan-11.jpeg"],
             template: "<img src='#: data #'>"
         });
         navigator.splashscreen.hide();
